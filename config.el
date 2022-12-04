624a782bd0eb48f912913db0fa0602d3afddf20f
Jānis Alunāns <j.alunans@gmail.com>
2022-12-04 23:53:26 +0200

    Add LSP and Emmet

--- config.el
+++ config.el
@@ -3,41 +3,50 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Jānis Alunāns"
       user-mail-address "j.alunans@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 20 :weight 'semi-light)
-       doom-variable-pitch-font (font-spec :family "monospace" :size 20))
-
-;;font family was sans replaced to monospace
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+(setq doom-font (font-spec :family "Fira Code" :size 18 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 19))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-ayu-mirage)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Org")
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 't)
+(setq display-line-numbers-type t)
 
-;;      CUSTOM CONFIG
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Org/")
 
+;; Comfort changes
+;;
 ;; Makes org-mode titles larger.
 (custom-set-faces
   '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
@@ -45,11 +54,9 @@
   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-  )
+)
 
-(global-visual-line-mode t)
-
-(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+(setq undo-limit 40000000                         ; Raise undo-limit to 40Mb
       evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
       auto-save-default t                         ; Nobody likes to loose work, I certainly don't
       truncate-string-ellipsis "…"                ; Unicode ellispis are nicer than "...", and also save /precious/ space
@@ -59,52 +66,66 @@
 
 (global-subword-mode 1)                           ; Iterate through CamelCase words
 
-(setq-default major-mode 'org-mode) ;;By default open files in Org mode.
-
-;; For nov to function.
-(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-;; Makes sure the day is monday.
+;; Monday is the first day of the week goddamnit, Americans!
 (setq calendar-week-start-day 1)
-(setq recentf-max-saved-items 300)
-;; The lower the number the more often you have repeat the cards
-(setq org-drill-learn-fraction 0.35)
-(setq org-drill-add-random-noise-to-intervals-p t)
-;; Sets the pomodoro sound
-(setq org-pomodoro-finished-sound "/home/higravia/Bin/beep.wav")
-(setq org-pomodoro-short-break-sound  "/home/higravia/Bin/beep.wav")
-(setq org-pomodoro-short-break-sound-p "/home/higravia/Bin/beep.wav")
-(setq org-pomodoro-length 20)
-(setq org-pomodoro-short-break-length 8)
 
-(map! :leader
-      :desc "Multiedit-replace"
-      "r" #'evil-multiedit-match-all)
+;;Does not complete quotes
+;;(sp-pair "'" nil :actions :rem)
+;;(sp-pair "\"" nil :actions :rem)
 
-(setq +zen-text-scale 0)
+;; Zen stuff
+(setq +zen-text-scale 0.2)
+
 
 (after! org
-        (add-to-list 'org-capture-templates
-                '("c" "Code"  entry
-                  (file "~/Org/code")
-                  "* Card :drill:\n%?\n** Code\n%i\n%l" :empty-lines 1))
+  (add-to-list 'org-capture-templates
+               '("c" "Card"  entry
+                 (file "~/Org/cards.org")
+                 "** Card :drill:\n%?\n*** Quote\n%i\n%l" :empty-lines 1)
+               )
+  (add-to-list 'org-capture-templates
+               '("s" "Slip"  entry
+                 (file "~/Org/slipbox.org")
+                 "** %?\n%i\n%l" :empty-lines 1)
+               )
   )
-;;(setq-default mode-line-format nil)
-;;(setq-default inhibit-splash-screen t)
-;;(setq-default hl-line-mode
-;;Does not complete quotes
-(sp-pair "'" nil :actions :rem)
-(sp-pair "\"" nil :actions :rem)
-;;(setq-default header-line-format " ")
-;;(set-frame-parameter nil 'internal-border-width 10)
-;; leader means the space
-;; desc is merely the description
-;; Quotes contain the keys
-;; After # is the command
-;;(map! :leader
-;;      :desc "Close buffer"
-;;      "ESC" #'doom/switch-to-scratch-buffer)
 
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Custome keybindings
+(map! :desc "Comment-Uncomment" :g "C-c C-c" #'comment-or-uncomment-region)
+
+(use-package! nov
+  :mode ("\\.epub\\'" . nov-mode)
+  :config
+  (setq nov-save-place-file (concat doom-cache-dir "nov-places")))
+
+(setq nov-text-width t)
+(setq visual-fill-column-center-text t)
+(add-hook 'nov-mode-hook 'visual-line-mode)
+(add-hook 'nov-mode-hook 'visual-fill-column-mode)
+
+(defun my-nov-font-setup ()
+  (face-remap-add-relative 'variable-pitch :family "Liberation Serif"
+                                           :height 1.2))
+(add-hook 'nov-mode-hook 'my-nov-font-setup)
+
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -117,6 +138,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
--- custom.el
+++ custom.el
@@ -3,59 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#1c1e1f" "#e74c3c" "#b6e63e" "#e2c770" "#268bd2" "#fb2874" "#66d9ef" "#d6d6d4"])
- '(custom-safe-themes
-   '("6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
- '(elfeed-feeds
-   '("https://going-medieval.com/feed/" "https://theness.com/neurologicablog/index.php/feed/rss/" "https://feeds.a.dj.com/rss/RSSWorldNews.xml" "https://ankino.xyz/trashfuture_blackwolf_feed.xml" "https://feed.theskepticsguide.org/feed/rss.aspx?feed=sgu" "https://feeds.buzzsprout.com/1064269.rss" "https://www.omnycontent.com/d/playlist/aaea4e69-af51-495e-afc9-a9760146922b/4a3ca742-9a68-4850-a727-ab790176c0e9/bf57384a-96e6-402e-90c6-ab790178fd59/podcast.rss" "https://citationsneeded.libsyn.com/rss" "https://capitalismhitshome.libsyn.com/rss" "https://podcasts.files.bbci.co.uk/p09q0dgp.rss" "https://anchor.fm/s/1cf1dbd4/podcast/rss" "https://anchor.fm/s/1d80328/podcast/rss" "https://feeds.buzzsprout.com/1004689.rss" "https://feed.podbean.com/tenthousandposts/feed.xml" "https://feed.podbean.com/entrylevelleft/feed.xml" "https://feeds.soundcloud.com/users/soundcloud:users:542584203/sounds.rss" "https://feelinggood.libsyn.com/rss" "https://narcocast.com/feed/podcast/"))
- '(exwm-floating-border-color "#323435")
- '(fci-rule-color "#555556")
- '(highlight-tail-colors
-   ((("#2b3222" "#b6e63e" "green")
-     . 0)
-    (("#233033" "#66d9ef" "brightcyan")
-     . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#fd971f"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#b6e63e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#525254"))
- '(objed-cursor-color "#e74c3c")
- '(pdf-view-midnight-colors (cons "#d6d6d4" "#1c1e1f"))
- '(rustic-ansi-faces
-   ["#1c1e1f" "#e74c3c" "#b6e63e" "#e2c770" "#268bd2" "#fb2874" "#66d9ef" "#d6d6d4"])
- '(vc-annotate-background "#1c1e1f")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#b6e63e")
-    (cons 40 "#c4db4e")
-    (cons 60 "#d3d15f")
-    (cons 80 "#e2c770")
-    (cons 100 "#ebb755")
-    (cons 120 "#f3a73a")
-    (cons 140 "#fd971f")
-    (cons 160 "#fc723b")
-    (cons 180 "#fb4d57")
-    (cons 200 "#fb2874")
-    (cons 220 "#f43461")
-    (cons 240 "#ed404e")
-    (cons 260 "#e74c3c")
-    (cons 280 "#c14d41")
-    (cons 300 "#9c4f48")
-    (cons 320 "#77504e")
-    (cons 340 "#555556")
-    (cons 360 "#555556")))
- '(vc-annotate-very-old-color nil))
-
+ '(auth-source-save-behavior nil)
+ '(warning-suppress-types '((initialization) (initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.1))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
-(put 'projectile-ag 'disabled nil)
